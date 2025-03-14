2a0cc0e00fce973e8a01585ed3d18259e64ee678
meeksfr <mika.sustar@mail.utoronto.ca>
2025-03-14 16:42:55 -0400

    init be/fe connection

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "xxx"
+
+@app.route("/test", methods=["GET", "POST"])
+def test():
+    if request.method == "POST":
+        some_json = request.get_json()
+        return jsonify({"you sent": some_json}), 201
+    else:
+        return jsonify({"about": "Hello World"}), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
